9169043a47aa8bd7e9cde9551d239bb997901e05
Tom White <tom.e.white@gmail.com>
2020-08-07 10:44:45 +0100

    Update rechunker example to work with latest version

--- examples/rechunk_cloud_storage_cloud_compute.py
+++ examples/rechunk_cloud_storage_cloud_compute.py
@@ -1,44 +1,84 @@
 # Rechunk a cloud Zarr file running Dask with Pywren
 
+import fsspec
+from numpy import prod
 import sys
 
-import gcsfs
 import zarr
 import dask.array as da
-
-from rechunker import api
-
 import pywren_ibm_cloud as pywren
-
 from dask_executor_scheduler import executor_scheduler, PywrenExecutor
+from rechunker import rechunk
 
 if __name__ == '__main__':
     
     args = sys.argv[1:]
-    if len(args) != 2:
-        print("Usage: python rechunk_cloud_storage_cloud_compute.py <project_id> <bucket>")
+    if len(args) != 3:
+        print("Usage: python rechunk_cloud_storage_cloud_compute.py <command> <project_id> <bucket>")
         sys.exit(1)
 
-    project_id = args[0]
-    bucket = args[1]
+    command, project_id, bucket = args
 
-    fs = gcsfs.GCSFileSystem(project=project_id)
-    source_store = gcsfs.mapping.GCSMap(f"{bucket}/source.zarr", gcs=fs)
-    target_store = gcsfs.mapping.GCSMap(f"{bucket}/target.zarr", gcs=fs)
-    temp_store = gcsfs.mapping.GCSMap(f"{bucket}/temp.zarr", gcs=fs)
+    source_url = f"gs://{bucket}/source.zarr"
+    target_url = f"gs://{bucket}/target.zarr"
+    temp_url = f"gs://{bucket}/temp.zarr"
+
+    source_store = fsspec.get_mapper(source_url)
+    target_store = fsspec.get_mapper(target_url)
+    temp_store = fsspec.get_mapper(temp_url)
 
     shape = (4000, 4000)
-    source_chunks = (200, 4000)
-    dtype = "f4"
+    source_chunks = (400, 4000)
+    target_chunks = (4000, 400)
 
-    a_source = zarr.ones(shape, chunks=source_chunks, dtype=dtype, store=source_store)
+    if command == "delete":
+        # delete everything
 
-    max_mem = 25_600_000
-    target_chunks = (4000, 200)
-    delayed = api.rechunk_zarr2zarr_w_dask(
-        a_source, target_chunks, max_mem, target_store, temp_store=temp_store
-    )
+        for url in (source_url, target_url, temp_url):
+            fs = fsspec.open(url).fs
+            if fs.exists(url):
+                fs.rm(url, recursive=True)
 
-    # Use conda runtime since pip needs a compiler to build zarr numcodecs
-    runtime = f"{project_id}/pywren-cloudrun-conda-v37:latest"
-    delayed.compute(scheduler=executor_scheduler, executor=PywrenExecutor(pywren.cloudrun_executor(runtime=runtime)), batch_size=10)
+    elif command == "create":
+        # create a random Zarr array for the source
+
+        # dask_chunks determine the unit of work - they are bigger than the chunks
+        # of the zarr file we are writing to
+        dask_chunks = (800, 4000)
+        zarr_chunks = source_chunks
+
+        # Use conda runtime since pip needs a compiler to build zarr numcodecs
+        runtime = f"{project_id}/pywren-cloudrun-conda-v37:latest"
+        executor = PywrenExecutor(pywren.cloudrun_executor(runtime=runtime))
+
+        arr = da.random.random(size=shape, chunks=dask_chunks)
+        itemsize = arr.dtype.itemsize
+        max_mem = str(itemsize * prod(dask_chunks))
+        plan = rechunk(arr, zarr_chunks, max_mem, source_store)
+
+        plan.execute(
+            scheduler=executor_scheduler,
+            executor=executor,
+            batch_size=100
+        )
+
+    elif command == "rechunk":
+        # rechunk the source to the target
+
+        max_mem = 25_600_000
+
+        # Use conda runtime since pip needs a compiler to build zarr numcodecs
+        runtime = f"{project_id}/pywren-cloudrun-conda-v37:latest"
+        executor = PywrenExecutor(pywren.cloudrun_executor(runtime=runtime))
+
+        source_array = zarr.open_array(source_store, mode="r")
+
+        plan = rechunk(
+            source_array, target_chunks, max_mem, target_store, temp_store=temp_store
+        )
+
+        plan.execute(
+            scheduler=executor_scheduler,
+            executor=executor,
+            batch_size=100
+        )
--- examples/rechunk_local_storage_local_compute.py
+++ examples/rechunk_local_storage_local_compute.py
@@ -1,31 +1,80 @@
-# Rechunk a local Zarr file running local Dask
+# Rechunk a local Zarr file running local Dask with Pywren
 
-from pathlib import Path
-import tempfile
+import fsspec
+from numpy import prod
+import sys
 
 import zarr
 import dask.array as da
-
-from rechunker import api
+import pywren_ibm_cloud as pywren
+from dask_executor_scheduler import executor_scheduler, PywrenExecutor
+from rechunker import rechunk
 
 if __name__ == '__main__':
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        #tmpdir = Path("tmp")
-        tmpdir = Path(tmpdirname)
 
-        source_store = str(tmpdir / "source.zarr")
-        shape = (4000, 4000)
-        source_chunks = (200, 4000)
-        dtype = "f4"
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print("Usage: python rechunk_local_storage_local_compute.py <command>")
+        sys.exit(1)
 
-        a_source = zarr.ones(shape, chunks=source_chunks, dtype=dtype, store=source_store)
+    command = args[0]
 
-        target_store = str(tmpdir / "target.zarr")
-        temp_store = str(tmpdir / "temp.zarr")
-        max_mem = 25_600_000
-        target_chunks = (4000, 200)
-        delayed = api.rechunk_zarr2zarr_w_dask(
-            a_source, target_chunks, max_mem, target_store, temp_store=temp_store
+    source_url = "data/source.zarr"
+    target_url = "data/target.zarr"
+    temp_url = "data/temp.zarr"
+
+    source_store = fsspec.get_mapper(source_url)
+    target_store = fsspec.get_mapper(target_url)
+    temp_store = fsspec.get_mapper(temp_url)
+
+    shape = (4000, 4000)
+    source_chunks = (400, 4000)
+    target_chunks = (4000, 400)
+
+    if command == "delete":
+        # delete everything
+
+        for url in (source_url, target_url, temp_url):
+            fs = fsspec.open(url).fs
+            if fs.exists(url):
+                fs.rm(url, recursive=True)
+
+    elif command == "create":
+        # create a random Zarr array for the source
+
+        # dask_chunks determine the unit of work - they are bigger than the chunks
+        # of the zarr file we are writing to
+        dask_chunks = (800, 4000)
+        zarr_chunks = source_chunks
+
+        executor = PywrenExecutor(pywren.local_executor())
+
+        arr = da.random.random(size=shape, chunks=dask_chunks)
+        itemsize = arr.dtype.itemsize
+        max_mem = str(itemsize * prod(dask_chunks))
+        plan = rechunk(arr, zarr_chunks, max_mem, source_store)
+
+        plan.execute(
+            scheduler=executor_scheduler,
+            executor=executor,
+            batch_size=100
         )
 
-        delayed.compute()
+    elif command == "rechunk":
+        # rechunk the source to the target
+
+        max_mem = 25_600_000
+
+        executor = PywrenExecutor(pywren.local_executor())
+
+        source_array = zarr.open_array(source_store, mode="r")
+
+        plan = rechunk(
+            source_array, target_chunks, max_mem, target_store, temp_store=temp_store
+        )
+
+        plan.execute(
+            scheduler=executor_scheduler,
+            executor=executor,
+            batch_size=100
+        )
